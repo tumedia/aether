cd961f5c4cded52ebb9d860300f50ae05d610b5a
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2018-03-16 14:51:59 +0100

    include more sentry options

--- src/Services/SentryService.php
+++ src/Services/SentryService.php
@@ -12,26 +12,31 @@ class SentryService extends Service
             return;
         }
 
-        $client = $this->getClient();
+        $client = $this->getClient()->install();
 
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $client->user_context([
                 'ip_address' => $_SERVER['HTTP_X_FORWARDED_FOR'],
             ]);
         }
-
-        $client->install();
     }
 
     protected function getClient()
     {
+        $projectRoot = rtrim($this->sl->get('projectRoot'), '/');
+
         return new Raven_Client(config('app.sentry.dsn'), [
+            'environment' => config('app.env'),
+            'app_path' => $projectRoot,
+            'prefixes' => [$projectRoot],
+            'excluded_app_paths' => ["{$projectRoot}/vendor"],
             'trace' => true,
             'curl_method' => 'sync',
             'curl_ipv4' => false,
             'trust_x_forwarded_proto' => true,
             'tags' => [
                 'php_version' => phpversion(),
+                'project_root' => $projectRoot,
             ],
         ]);
     }
