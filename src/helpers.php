c450ab772fc34bc5799db674a0ea281de27cb532
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2018-03-27 16:53:08 +0200

    now using env() in illuminate/support

--- src/helpers.php
+++ src/helpers.php
@@ -34,48 +34,6 @@ if (!function_exists('resolve')) {
     }
 }
 
-if (!function_exists('env')) {
-    /**
-     * Get an environment variable.
-     *
-     * @param  string $key
-     * @param  mixed  $default = null
-     * @return mixed  Returns the value of `$default` if the environment
-     *                variable is not set.
-     */
-    function env(string $key, $default = null)
-    {
-        $value = getenv($key);
-
-        if ($value === false) {
-            return $default;
-        }
-
-        switch (strtolower($value)) {
-            case 'true':
-            case '(true)':
-                return true;
-            case 'false':
-            case '(false)':
-                return false;
-            case 'empty':
-            case '(empty)':
-                return '';
-            case 'null':
-            case '(null)':
-                return;
-        }
-
-        $valueLength = strlen($value);
-
-        if ($valueLength > 1 && strpos($value, '"') === 0 && strrpos($value, '"') === $valueLength - 1) {
-            return substr($value, 1, -1);
-        }
-
-        return $value;
-    }
-}
-
 if (!function_exists('config')) {
     /**
      * Get the config repository instance, or if `$key` is set, return the
