12b43a2204084c0ecb0e544c15e9b2bfb44e2095
Erlend Blakstad <erlend@tumedia.no>
2018-06-26 11:12:17 +0200

    remove custom has check for service locator

--- src/ServiceLocator.php
+++ src/ServiceLocator.php
@@ -60,20 +60,6 @@ class ServiceLocator extends Container
     }
 
     /**
-     * Check if an entry exists
-     * Nullcheck for backwards compatibility
-     */
-    public function has($name)
-    {
-        $result = parent::has($name);
-        if ($result) {
-            $value = parent::resolve($name);
-            return $value === null ? false : true;
-        }
-        return false;
-    }
-
-    /**
      * Resolve a vector object.
      *
      * @param  string  $name
