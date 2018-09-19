4ff0dba4507b195e51edf707380ffd97da575c02
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2018-09-19 11:14:27 +0200

    Add a super useful hint

--- src/Assets/AssetNotFoundException.php
+++ src/Assets/AssetNotFoundException.php
@@ -8,6 +8,12 @@ class AssetNotFoundException extends Exception
 {
     public static function forAsset($asset, $assetPath)
     {
-        return new static("Asset [{$asset}] not found at [{$assetPath}]");
+        $message = "Asset [{$asset}] not found at [{$assetPath}]";
+
+        if (! app()->isProduction()) {
+            $message .= "\n\nHint: Try running \"npm run watch\"";
+        }
+
+        return new static($message);
     }
 }
